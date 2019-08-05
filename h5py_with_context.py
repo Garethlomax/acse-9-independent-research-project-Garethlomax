52a8ddf7666ef56db038b04227119f5bd330750f
Gareth Lomax <gcl15@ic.ac.uk>
2019-08-05 23:50:02 +0100

    fixing gcp_ppp bug

--- h5py_with_context.py
+++ h5py_with_context.py
@@ -12,8 +12,25 @@ import seaborn as sns
 import h5py
 
 test_dat = np.zeros(10)
-with h5py.File("simple_attribute_test3.hdf5",'w') as t:
+with h5py.File("simple_attribute_test4.hdf5",'w') as t:
 
     t.create_dataset("main", data = test_dat)
     key_list = [u"gcp_ppp", u"petroleum_y",u"drug_y", u"prec_gpcp"]
     t["main"].attrs.create("key_prio", np.string_(key_list))
+
+
+def find_avg_lazy_load(filename, key_list):
+    # file name is name of hdf5 file
+    data = h5py.File(filename, 'r')
+    predictor = data["predictor"]
+    channel_num = predictor.shape[2]
+    avg = np.zeros(channel_num)
+    std = np.zeros_like(avg)
+    for i in range(channel_num):
+        avg = np.avg(predictor[:,:,i])
+        std = np.avg(predictor[:,:,i])
+    return avg, std
+
+
+
+
